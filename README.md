b4f3d5f985e47c2ee4abcd99d36b4754a62f3cbd
Franciszek Madej <f.madej@protonmail.com>
2019-09-13 20:17:49 +0200

    initial commit with step one

---
+++ README.md
@@ -0,0 +1,25 @@
+# Mycroft-NXT-Enclosure
+
+# First step:
+
+get object detection working, recognize person and then use it in next steps.
+
+![./before.jpg](without object recognition)
+![./after.jpeg](with object recognition)
+
+## Second step:
+
+calculate object offset to rotate camera using NXT motors
+
+## Third step:
+
+connect to Mycroft Voice Assistant API
+
+### Sources used:
+- https://towardsdatascience.com/detecting-faces-with-python-and-opencv-face-detection-neural-network-f72890ae531c
+- https://www.pyimagesearch.com/2017/09/18/real-time-object-detection-with-deep-learning-and-opencv/
+
+### Libraries used:
+
+- [nxt-python](https://github.com/Eelviny/nxt-python)
+- [opencv-python](https://pypi.org/project/opencv-python/)
---
+++ main.py
@@ -0,0 +1,78 @@
+import numpy as np
+import time
+import cv2
+from imutils.video import VideoStream
+import imutils
+
+import logging
+import chromalog
+from chromalog.mark.helpers.simple import success
+
+chromalog.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+
+logger.debug("loading model")
+# model used was made by generous folks over at pyimagesearch.com. thanks so much!
+net = cv2.dnn.readNetFromCaffe("./prototext.txt", "./model.caffemodel")
+logger.debug("model loaded!")
+
+# load the input image and construct an input blob for the image
+# by resizing to a fixed 300x300 pixels and then normalizing it
+# (note: normalization is done via the authors of the MobileNet SSD
+# implementation)
+logger.debug("connecting to webcam...")
+vs = VideoStream(src=0).start()
+time.sleep(2)
+
+# pass the blob through the network and obtain the detections and
+# predictions
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(frame, (300, 300)), 0.007843, (300, 300), 127.5
+    )
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    net.setInput(blob)
+    detections = net.forward()
+
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence > 0.2:
+            # extract the index of the class label from the
+            # `detections`, then compute the (x, y)-coordinates of
+            # the bounding box for the object
+            idx = int(detections[0, 0, i, 1])
+            PERSON_INDEX = 15  # other indexes are other objects (bottles etc)
+            if idx == PERSON_INDEX:
+                logger.debug("found a person in range. calculating offset...")
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                middleX = endX - (endX - startX) / 2
+                middleY = endY - (endY - startY) / 2
+
+                offsetX = w / 2 - middleX
+                offsetY = h / 2 - middleY
+
+                logger.debug("offset: X: %s Y: %s", success(offsetX), success(offsetY))
+                if abs(offsetY) < 5 and abs(offsetX) < 5:
+                    logger.debug("position is perfect!")
+                else:
+                    # rotate here
+                    logger.debug("adjusting position...")
+
+
+cv2.destroyAllWindows()
+vs.stop()
